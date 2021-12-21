2a0b97e68bedc43a592cee7d82100caf4d4f6d3a
ado8 <ado8@bernese.cs.jhu.edu>
2021-12-20 21:04:12 -0500

    script for backup checkpoints and load cnn pretrained

--- tanksworld/run.sh
+++ tanksworld/run.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-
+bk_checkpoint(){
+    DIRNAME=results/$1/checkpoints
+    ARR=()
+    for FILE in $(ls $DIRNAME)
+    do
+        IFS='_' read -ra array <<< "$FILE"
+        ARR+=(${array[2]})
+    done
+    #echo ${ARR]}|sort -r
+    IFS=$'\n'
+    cp -r results/$1 results_backup
+    LAST_CHECKPOINT=$(echo "${ARR[*]}"|sort -r|head -n1)
+    for NUM in ${ARR[@]}
+    do
+        if [[ $NUM != ${LAST_CHECKPOINT} ]]; then
+            trash "${DIRNAME}/rl_model_${NUM}_steps.zip"
+        fi
+    done
+}
 garage(){
     xvfb-run -s "-screen 0 1280x1024x24" --auto-servernum --server-num=1 python garage_test.py
 }
@@ -19,27 +37,27 @@ debug(){
 train(){
     python centralized.py --exe /home/ado8/ai-safety-challenge/exe/aisafetytanks_017_headless/aisafetytanks_017_headless.x86_64 \
         --save-freq 5000 --n-steps $1 --n-envs $2 --timestep $3 --penalty-weight $4 --ent-coef $5 --config $6 --env-timeout $7\
+        --save-path results/21-12-20-11:56:29TW-timestep7.0M-nstep64-nenv20-timeout-500-neg-0.4-lrtype-constant-1vs1-random-tank/checkpoints/rl_model_2600000_steps.zip \
+        --freeze-cnn --load-type cnn\
         --lr 0.0001 --lr-type constant --training 
 }
-
-rm_checkpoint(){
-    DIRNAME=results/$1/checkpoints
-    ARR=()
+record(){
+    SETTING=$1
+    EXPMNT=results/$SETTING
+    DIRNAME=/home/ado8/ai-safety-challenge/tanksworld/$EXPMNT/checkpoints
+    SAVEDIR=tmp/tank_vid/$SETTING
+    mkdir -p $SAVEDIR
     for FILE in $(ls $DIRNAME)
     do
-        IFS='_' read -ra array <<< "$FILE"
-        ARR+=(${array[2]})
-    done
-    #echo ${ARR]}|sort -r
-    IFS=$'\n'
-    LAST_CHECKPOINT=$(echo "${ARR[*]}"|sort -r|head -n1)
-    for NUM in ${ARR[@]}
-    do
-        if [[ $NUM != ${LAST_CHECKPOINT} ]]; then
-            trash "${DIRNAME}/rl_model_${NUM}_steps.zip"
-        fi
+        echo $FILE
+        echo $SAVEDIR/$FILE.avi
+        python centralized.py --exe /home/ado8/ai-safety-challenge/exe/aisafetytanks_017_headless/aisafetytanks_017_headless.x86_64 \
+            --save-path $DIRNAME/$FILE \
+            --n-env 1 --penalty-weight 0.2 --timestep 4000000 \
+            --video-path $SAVEDIR/$FILE.avi --n-episode $2 --record
     done
 }
+
 if [[ $1 == test ]]; then
     test
 elif [[ $1 == train ]]; then
@@ -49,9 +67,9 @@ elif [[ $1 == debug ]]; then
     debug 64 10 700 0.0 0.00 2 
 elif [[ $1 == debug-dummy ]]; then
     python centralized.py --exe /home/ado8/ai-safety-challenge/exe/aisafetytanks_017_headless/aisafetytanks_017_headless.x86_64 \
-        --save-freq 20000   --n-steps 32 --n-envs 2 --timestep 1000000 --penalty-weight 0.6 --config 2 --training --debug --dummy-proc  --lr-type linear \
-        #--save-path results/21-12-17-21:51:56TW-timestep7.0M-nstep64-nenv20-timeout-500-neg-0.4-lrtype-constant-random-tank-5/checkpoints/rl_model_700000_steps.zip\
-        #--freeze-cnn
+        --save-freq 20000 --n-steps 32 --n-envs 20 --timestep 1000000 --penalty-weight 0.6 --config 2 --training --debug --dummy-proc  --lr-type linear \
+        --save-path results/21-12-20-11:56:29TW-timestep7.0M-nstep64-nenv20-timeout-500-neg-0.4-lrtype-constant-1vs1-random-tank/checkpoints/rl_model_2600000_steps.zip \
+        --freeze-cnn
 elif [[ $1 == debug-gym ]]; then
     debug_gym 128 5 50000000  0.3
 elif [[ $1 == record ]]; then
@@ -71,24 +89,11 @@ elif [[ $1 == record-full-step ]]; then
             --video-path $SAVEDIR/$STEP.avi --n-episode 10 --record
     done
 elif [[ $1 == record-full ]]; then
-    SETTING=21-12-17-10:19:42TW-timestep7.0M-nstep64-nenv20-timeout-250-neg-0.4-lrtype-constant-froze,no-shooting-tank-1-9
-    EXPMNT=results/$SETTING
-    DIRNAME=/home/ado8/ai-safety-challenge/tanksworld/$EXPMNT/checkpoints
-    SAVEDIR=tmp/tank_vid/$SETTING
-    mkdir -p $SAVEDIR
-    for FILE in $(ls $DIRNAME)
-    do
-        echo $FILE
-        echo $SAVEDIR/$FILE.avi
-        python centralized.py --exe /home/ado8/ai-safety-challenge/exe/aisafetytanks_017_headless/aisafetytanks_017_headless.x86_64 \
-            --save-path $DIRNAME/$FILE \
-            --n-env 1 --penalty-weight 0.2 --timestep 4000000 \
-            --video-path $SAVEDIR/$FILE.avi --n-episode 10 --record
-    done
-elif [[ $1 == rm-checkpoints ]]; then
+    record
+elif [[ $1 == bk-checkpoints ]]; then
     for DIR in $(ls results)
     do
-        rm_checkpoint $DIR
+        bk_checkpoint $DIR
     done
 elif [[ $1 == process ]]; then
     ps aux | grep ai-safety-challenge
@@ -104,4 +109,3 @@ elif [[ $1 == lab ]]; then
 else
     echo "no matching command"
 fi
-
