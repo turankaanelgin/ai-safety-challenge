605e9a3b0013e5eac6ecd9f022484792ab6b6186
Kaan Elgin <kelgin@r09>
2022-01-08 14:56:41 -0500

    Multiplayer

--- tanksworld/my_main_script_updated.py
+++ tanksworld/my_main_script_updated.py
@@ -4,6 +4,7 @@ import os
 import pdb
 import sys
 import argparse
+from tensorboardX import SummaryWriter
 
 import numpy as np
 
@@ -13,6 +14,7 @@ from algos.torch_ppo.mappo_gpu_new import PPOPolicy as TorchGPUMAPPOPolicyNew
 from algos.torch_ppo.mappo_gpu_multiplayer import PPOPolicy as MAPPOMultiPlayer
 from algos.torch_ppo.mappo_gpu_curiosity import PPOPolicy as MAPPOCuriosity
 from algos.torch_sac.sac_new import SACPolicy
+from algos.maddpg.maddpg import MADDPGPolicy
 from algos.torch_ppo.callbacks import EvalCallback
 from algos.torch_ppo.vec_env import DummyVecEnv, SubprocVecEnv
 
@@ -32,6 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('--value_lr', type=float, default=1e-3)
     parser.add_argument('--policy_lr_schedule', type=str, default='cons')
     parser.add_argument('--value_lr_schedule', type=str, default='cons')
+    parser.add_argument('--entropy_coef', type=float, default=0.0)
     parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--death_penalty', action='store_true', default=False)
     parser.add_argument('--friendly_fire', action='store_true', default=True)
@@ -47,6 +50,7 @@ if __name__ == '__main__':
     parser.add_argument('--freeze_rep', action='store_true', default=False)
     parser.add_argument('--use_rnn', action='store_true', default=False)
     parser.add_argument('--eval_logdir', type=str, default='')
+    parser.add_argument('--multiplayer', action='store_true', default=False)
     args = parser.parse_args()
 
     config = vars(args)
@@ -61,6 +65,10 @@ if __name__ == '__main__':
                                                                          args.save_tag)
     if config['curriculum_start'] >= 0.0:
         folder_name += '__CS={}__CF={}'.format(config['curriculum_start'], config['curriculum_stop'])
+    if config['entropy_coef'] > 0.0:
+        folder_name += '__ENT={}'.format(config['entropy_coef'])
+    if config['multiplayer']:
+        folder_name += '__MULTI'
     eval_folder_name = folder_name
     if args.eval_mode:
         eval_folder_name += '__EVAL/{}'.format(args.eval_checkpoint)
@@ -72,8 +80,9 @@ if __name__ == '__main__':
     else:
         env_seed = args.env_seed
 
-    stats_dir = './runs/stats_{}'.format(args.logdir)
+    stats_dir = os.path.join('./logs', args.logdir, folder_name, 'stats')
     os.makedirs(stats_dir, exist_ok=True)
+    tb_writer = SummaryWriter(stats_dir)
 
     if args.eval_mode:
         eval_seed_folder = os.path.join('./logs', args.logdir, 'eval_seeds.json')
@@ -102,14 +111,20 @@ if __name__ == '__main__':
         env = SubprocVecEnv(stacked_env)
 
     else:
+        if args.multiplayer:
+            random_tanks = []
+            num_agents = 10
+        else:
+            random_tanks = [5,6,7,8,9]
+            num_agents = 5
         env_kwargs = {'exe': args.exe,
-                     'static_tanks': [], 'random_tanks': [5,6,7,8,9], 'disable_shooting': [],
+                     'static_tanks': [], 'random_tanks': random_tanks, 'disable_shooting': [],
                      'friendly_fire': True, 'kill_bonus': False, 'death_penalty': False,
-                     'take_damage_penalty': True, 'tblogs': stats_dir,
+                     'take_damage_penalty': True, 'tblogs': stats_dir, 'tb_writer': tb_writer,
                      'penalty_weight': config['penalty_weight'], 'reward_weight': 1.0,
                      'friendly_fire_weight': config['ff_weight'], 'timeout': 500, 'log_statistics': True,
-                     'seed': args.env_seed, 'curriculum_stop': config['curriculum_stop'], 'curriculum_steps': args.num_iter}
-        env = DummyVecEnv([lambda : make_env(**env_kwargs)])
+                     'seed': args.env_seed, 'curriculum_stop': config['curriculum_stop'], 'curriculum_steps': args.num_iter,}
+        env = DummyVecEnv([lambda : make_env(**env_kwargs)], num_agents)
 
     if args.eval_mode:
         policy_record = PolicyRecord(eval_folder_name, './logs/' + args.logdir + '/')
@@ -125,18 +140,22 @@ if __name__ == '__main__':
     elif args.eval_mode:
         model_path = os.path.join('./logs', args.eval_logdir, folder_name, 'checkpoints', model_id)
         model_path = os.path.join(model_path, '{}.pth'.format(args.eval_checkpoint))
-    '''
+
     policy_kwargs = {
         'steps_per_epoch': config['batch_size'],
         'train_pi_iters': 4,
         'train_v_iters': 4,
         'pi_lr': config['policy_lr'],
         'vf_lr': config['value_lr'],
-        'ent_coef': -1.0,
+        'ent_coef': config['entropy_coef'],
         'pi_scheduler': config['policy_lr_schedule'],
         'vf_scheduler': config['value_lr_schedule'],
         'seed': args.policy_seed,
         'cnn_model_path': './models/frozen-cnn-0.8/4000000.pth',
+        'enemy_model_path': '/cis/net/r09_ssd/data/kelgin/final-baseline-v2-bernese/'+\
+                            'lrp=0.0003cons__lrv=0.001cons__r=1.0__p=0.5__ff=0.0__H=64__/seed0/checkpoints/'+
+                            'final-baseline-v2---lrp=0.0003cons__lrv=0.001cons__r=1.0__p=0.5__ff=0.0__H=64__/'+
+                            'seed0-0-/999999.pth',
         'model_path': model_path,
         'n_envs': 1,
         'model_id': model_id,
@@ -144,14 +163,15 @@ if __name__ == '__main__':
         'freeze_rep': args.freeze_rep,
         'use_rnn': args.use_rnn,
         'num_states': 5,
+        'tb_writer': tb_writer,
     }
 
     callback = EvalCallback(env, policy_record, eval_env=None)
-    policy = TorchGPUMAPPOPolicyNew(env, callback, args.eval_mode, **policy_kwargs)
-    #policy = MAPPOMultiPlayer(env, callback, True, **policy_kwargs)
+    #policy = TorchGPUMAPPOPolicyNew(env, callback, args.eval_mode, **policy_kwargs)
+    policy = MAPPOMultiPlayer(env, callback, args.eval_mode, **policy_kwargs)
     policy.run(num_steps=args.num_iter)
-    '''
 
+    '''
     policy_kwargs = {
         'steps_per_epoch': config['batch_size'],
         'seed': args.policy_seed,
@@ -164,6 +184,8 @@ if __name__ == '__main__':
     }
     
     callback = EvalCallback(env, policy_record, eval_env=None)
-    policy = SACPolicy(env, callback, args.eval_mode, **policy_kwargs)
-    #policy = MAPPOCuriosity(env, callback, args.eval_mode, **policy_kwargs)
+    #policy = SACPolicy(env, callback, args.eval_mode, **policy_kwargs)
+    policy = MAPPOCuriosity(env, callback, args.eval_mode, **policy_kwargs)
+    #policy = MADDPGPolicy(env, callback, args.eval_mode, **policy_kwargs)
     policy.run(num_steps=args.num_iter)
+    '''
