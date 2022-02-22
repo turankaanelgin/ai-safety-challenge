69b7b80a186c85878bcd601ad87d720b82e4fc60
telgin1 <telgin1@bernese.cs.jhu.edu>
2022-02-22 14:58:42 -0500

    Noisy parameters

--- tanksworld/trainer_config.py
+++ tanksworld/trainer_config.py
@@ -46,6 +46,11 @@ parser.add_argument('--cnn_path', type=str, default='./models/frozen-cnn-0.8/400
 parser.add_argument('--weight_sharing', action='store_true', default=False)
 parser.add_argument('--curiosity', action='store_true', default=False)
 parser.add_argument('--central_critic', action='store_true', default=False)
+parser.add_argument('--param_noise', action='store_true', default=False)
+parser.add_argument('--value_clip', action='store_true', default=False)
+parser.add_argument('--rollback', action='store_true', default=False)
+parser.add_argument('--trust_region', action='store_true', default=False)
+parser.add_argument('--reward_norm', action='store_true', default=False)
 # TODO this argument is temporary until fixing a bug
 parser.add_argument('--seed_idx', type=int)
 
